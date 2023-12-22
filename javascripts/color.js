8385af4305a34433d99701f9c4cc0a17578906e7
ArolaunTech <137230773+ArolaunTech@users.noreply.github.com>
2023-12-21 19:49:39 -0800

    RGB <-> XYZ

--- javascripts/color.js
+++ javascripts/color.js
@@ -1,5 +1,7 @@
 //Handles color conversions and other functions
 //RGB will be provided in 0-1 format
+//Sources:
+// - https://en.wikipedia.org/wiki/SRGB
 
 function linRtosR(r) {
   //Helper function for linRGBtosRGB(r, g, b)
@@ -25,4 +27,14 @@ function sRtolinR(r) {
 function sRGBtolinRGB(rgb) {
   //Converts sRGB to linear RGB
   return [sRtolinR(rgb[0]), sRtolinR(rgb[1]), sRtolinR(rgb[2])];
+}
+
+function linRGBtoXYZ(rgb) {
+  //Converts linear RGB to CIE XYZ (D65 whitepoint)
+  return [0.4124*rgb[0] + 0.3576*rgb[1] + 0.1805*rgb[2], 0.2126*rgb[0] + 0.7152*rgb[1] + 0.0722*rgb[2], 0.0192*rgb[0] + 0.1192*rgb[1] + 0.9505*rgb[2]];
+}
+
+function XYZtolinRGB(xyz) {
+  //Converts CIE XYZ (D65 whitepoint) to linear RGB
+  return [3.2406255*xyz[0] - 1.537208*xyz[1] - 0.4986286*xyz[2], -0.9689307*xyz[0] + 1.8757561*xyz[1] + 0.0415175*xyz[2], 0.0557101*xyz[0] - 0.2040211*xyz[1] + 1.0569959*xyz[2]];
 }
