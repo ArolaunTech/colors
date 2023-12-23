8d9efdf49453bbab2024cd9aa4073a982e0d57a5
ArolaunTech <137230773+ArolaunTech@users.noreply.github.com>
2023-12-22 19:32:05 -0800

    helper function - save

--- javascripts/color.js
+++ javascripts/color.js
@@ -12,7 +12,17 @@ for (var i = 0; i < colorspaces.length; i++) {
     if (i == j) {
       continue;
     }
-    graph[colorspaces[i]][colorspaces[j]] = '';
+    graph[colorspaces[i]][colorspaces[j]] = [];
   }
 }
 console.log(graph);
+
+//Color conversion functions
+function linRtosR(r) {
+  //Helper function for RGBtosRGB
+  if (r < 0.0031308) {
+    return 12.92*r;
+  }
+  return 1.055*Math.pow(r, 5/12)-0.055;
+}
+
